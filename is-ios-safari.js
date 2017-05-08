3a846316b25a49b5f54ccd047f25c3057ff6e0cd
lilaconlee and richardshin <code@getbraintree.com>
2017-05-08 21:40:08 +0000

    Update is-ios-safari to reorganize code

--- is-ios-safari.js
+++ is-ios-safari.js
@@ -2,12 +2,12 @@
 
 var isIos = require('./is-ios');
 
+function isWebkit(ua) {
+  return ua.match(/WebKit/i);
+}
+
 module.exports = function isIosSafari(ua) {
   ua = ua || navigator.userAgent;
 
   return isIos(ua) && isWebkit(ua) && ua.indexOf('CriOS') === -1;
 };
-
-function isWebkit(ua) {
-  return ua.match(/WebKit/i);
-}
