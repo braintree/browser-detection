c71741f05e15051bccf11b755b6c419415fe1ca5
Blade Barringer <blade.barringer@getbraintree.com>
2021-09-28 14:37:47 +0000

    refactor: extract ua parsing logic into named functions

--- src/is-ios-safari.ts
+++ src/is-ios-safari.ts
@@ -7,14 +7,22 @@ function isWebkit(ua: string): boolean {
   return webkitRegexp.test(ua);
 }
 
+function isIosChrome(ua: string): boolean {
+  return ua.indexOf("CriOS") > -1;
+}
+
+function isFacebook(ua: string): boolean {
+  return ua.indexOf("FBAN") > -1;
+}
+
 export = function isIosSafari(ua?: string): boolean {
   ua = ua || window.navigator.userAgent;
 
   return (
     isIos(ua) &&
     isWebkit(ua) &&
-    ua.indexOf("CriOS") === -1 &&
+    !isIosChrome(ua) &&
     !isIosFirefox(ua) &&
-    ua.indexOf("FBAN") === -1
+    !isFacebook(ua)
   );
 };
