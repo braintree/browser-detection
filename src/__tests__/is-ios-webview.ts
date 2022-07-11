505139f93e4710f09e9c39dea6e2277ec1815c4b
cdibble <cdibble@paypal.com>
2022-07-11 14:40:18 -0700

    remove mobile from regex since that doesnt mandate it being a webview

--- src/__tests__/is-ios-webview.ts
+++ src/__tests__/is-ios-webview.ts
@@ -4,8 +4,8 @@ const AGENTS: {
   [key: string]: string;
 } = require("./helpers/user-agents.json");
 
-describe.only("isIosWebview", () => {
-  it.only("returns true for iOS webviews", () => {
+describe("isIosWebview", () => {
+  it("returns true for iOS webviews", () => {
     let key, ua;
 
     for (key in AGENTS) {
@@ -15,7 +15,7 @@ describe.only("isIosWebview", () => {
       if (/webview/i.test(key) && !/android/i.test(key)) {
         ua = AGENTS[key];
         console.log("key", key);
-        
+
         expect(isIosWebview(ua)).toBe(true);
       }
     }
@@ -38,33 +38,20 @@ describe.only("isIosWebview", () => {
   it("returns false for desktop Safari", () => {
     expect(isIosWebview(AGENTS.macSafari7_0_2)).toBe(false);
   });
-  
+
   it("returns true when supported and not case-sensitive", () => {
     expect(isIosWebview(AGENTS.iPhoneWebviewLowercase)).toBe(true);
   });
 
-  it.skip("returns true merchantSuppliedSlackWebview example", () => {
-    // This is a merchant-supplied user agent that exhibited the issue they have. 
-    // It appears that the user agent can return with `Safari` in it even if its a webview. Which breaks our regex since it asserts it
-    // is a webview if it doesn't have safar after AppleWebKit
-    const merchantSuppliedSlackWebview = "mozilla/5.0 (linux; android 12; pixel 3a) applewebkit/537.36 (khtml, like gecko) chrome/101.0.4951.61 mobile safari/537.36"
-    expect(isIosWebview(merchantSuppliedSlackWebview)).toBe(true);
+  it("returns false when using a browser on an iPhone example", () => {
+    expect(isIosWebview(AGENTS.iPhone_15_5Safari)).toBe(false);
   });
 
-  it("returns true safariBrowserOnIphone example", () => {
-    // This is a merchant-supplied user agent that exhibited the issue they have. 
+  it.skip("returns true slackWebviewOnIphone example", () => {
+    // This is a merchant-supplied user agent that exhibited the issue they have.
     // It appears that the user agent can return with `Safari` in it even if its a webview. Which breaks our regex since it asserts it
     // is a webview if it doesn't have safar after AppleWebKit
-    const safariBrowserOnIphone = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/103.0.5060.63 Mobile/15E148 Safari/604.1"
-    expect(isIosWebview(safariBrowserOnIphone)).toBe(false);
-  });
-
-  it("returns true slackWebviewOnIphone example", () => {
-    // This is a merchant-supplied user agent that exhibited the issue they have. 
-    // It appears that the user agent can return with `Safari` in it even if its a webview. Which breaks our regex since it asserts it
-    // is a webview if it doesn't have safar after AppleWebKit
-    const slackWebviewOnIphone = "Mozilla/5.0 (iPhone; CPU iPhone OS 15 5 like Mac OS X) AppleWebKit/605.1.15(KHTML like Gecko) Version/15.5 Mobile/15E148 Safari/604.1"
-    expect(isIosWebview(slackWebviewOnIphone)).toBe(true);
+    expect(isIosWebview(AGENTS.iPhone_15_5_Webview)).toBe(true);
   });
 
   it("returns false for non-webviews", () => {
--- src/is-ios-webview.ts
+++ src/is-ios-webview.ts
@@ -4,14 +4,19 @@ import isIosGoogleSearchApp = require("./is-ios-google-search-app");
 export = function isIosWebview(ua?: string): boolean {
   ua = ua || window.navigator.userAgent;
   console.log("isIos(ua)", isIos(ua));
-  
+
   if (isIos(ua)) {
     // The Google Search iOS app is technically a webview and doesn't support popups.
     if (isIosGoogleSearchApp(ua)) {
       return true;
     }
-    
-    return /.+AppleWebKit(?!.*Safari)/i.test(ua) || /(AppleWebKit).*?(Version)(?!.*Mobile)/i.test(ua);
+    console.log(`ua`, ua);
+    console.log(`/.+AppleWebKit(?!.*Safari)/i.test(ua)`, /.+AppleWebKit(?!.*Safari)/i.test(ua));
+    console.log(`/(AppleWebKit).*?(Version)(?!.*Mobile)/i.test(ua)`, /(AppleWebKit).*?(Version)(?!.*Mobile)/i.test(ua));
+    return (
+      /.+AppleWebKit(?!.*Safari)/i.test(ua) ||
+      /(AppleWebKit).*?(Version)/i.test(ua)
+    );
   }
 
   return false;
